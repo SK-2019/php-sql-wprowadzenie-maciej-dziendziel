f7df529d79ee24f2037b96d759d7ad75fce54a35
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2020-12-14 17:05:18 +0100

    Add files via upload

---
+++ biblioteka.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>Document</title>
+</head>
+<body>
+<div class=nav>
+    <a href="pracownicy.php">Pracownicy</a>
+    <a href="organizacja.php">Pracownicy i Organizacja</a>
+    <a href="funkcje.php">Funkcje Agregujące</a>
+    <a href="dataczas.php">Data i Czas</a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-BilinskiJakub">Github</a>
+    <a href="formularz.html">Zakładka testowa</a>
+    <a href="daneDoBazy.php">Dane Do Bazy</a>
+    <a href="biblioteka.php">biblioteka</a>
+    
+
+
+</div>
+<?php
+
+require("connect.php");
+$sql = ('SELECT * FROM bibliotekaAutor');
+echo("<h2>Autorzy</h2>");
+echo("<h3>".$sql."</h3>");
+    $result = $conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>Autor</th>");
+    while($row=$result->fetch_assoc()){
+        echo("<tr>");
+            echo("<td>".$row["id_autor"]."</td><td>".$row["Autor"]."</td>");
+        echo("</tr>");
+    
+
+    }
+
+    echo("</table>");
+
+require("connect.php");
+$sql = ('SELECT * FROM bibliotekaTytuł');
+echo("<h2>Tytuły</h2>");
+echo("<h3>".$sql."</h3>");
+    $result = $conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>Tytuł</th>");
+    while($row=$result->fetch_assoc()){
+        echo("<tr>");
+            echo("<td>".$row["id_tytuł"]."</td><td>".$row["Tytuł"]."</td>");
+        echo("</tr>");
+    
+
+    }
+
+    echo("</table>");
+
+require("connect.php");
+$sql = ('SELECT * FROM `bibliotekaAT`, `bibliotekaAutor`, `bibliotekaTytuł` WHERE id_autor = bibliotekaAutor_ID AND id_tytuł = bibliotekaTytul_ID');
+echo("<h2>Dane w Bibliotece</h2>");
+echo("<h3>".$sql."</h3>");
+    $result = $conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>Autor</th>");
+        echo("<th>Tytuł</th>");
+    while($row=$result->fetch_assoc()){
+        echo("<tr>");
+            echo("<td>".$row["id"]."</td><td>".$row["Autor"]."</td><td>".$row["Tytuł"]);
+        echo("</tr>");
+    
+
+    }
+
+    echo("</table>");
+
+
+?>
+    
+</body>
+</html>
