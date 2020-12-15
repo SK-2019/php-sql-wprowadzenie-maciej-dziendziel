064eb8aab606fb93926afe9e463bd3410c61574a
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2020-12-15 20:23:26 +0000

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>Insert</title>
+</head>
+<body>
+
+<div class=nav>
+    <a href="pracownicy.php">Pracownicy</a>
+    <a href="organizacja.php">Pracownicy i Organizacja</a>
+    <a href="funkcje.php">Funkcje Agregujące</a>
+    <a href="dataczas.php">Data i Czas</a>
+    <a href="formularz.html">Zakładka testowa</a>
+    <a href="daneDoBazy.php">Dane Do Bazy</a>
+    <a href="biblioteka.php">Książki</a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel">Github</a>
+
+
+
+</div>
+<?php
+
+
+echo("<h2> Imie:".$_POST["imie"]."</h2>");
+echo("<h2> Dział:".$_POST["dzial"]."</h2>");
+echo("<h2> Zarobki:".$_POST["zarobki"]."</h2>");
+echo("<h2> Data Urodzenia:".$_POST["data_urodzenia"]."</h2>");
+
+
+require("connect.php");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+  $sql = ("INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki, data_urodzenia) VALUES (NULL,'".$_POST['imie']."', ".$_POST['dzial'].", ".$_POST['zarobki'].",'".$_POST['data_urodzenia']."')");
+  echo "<li>".$sql;
+  
+  
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+
+  require("connect.php");
+    $sql = ('SELECT * FROM pracownicy');
+    echo("<h2>Pracownicy</h2>");
+    echo("<h3>".$sql."</h3>");
+        $result = $conn->query($sql);
+            echo("<table border=1>");
+            echo("<th>ID</th>");
+            echo("<th>Imie</th>");
+            echo("<th>Zarobki</th>");
+            echo("<th>Data_Urodzenia</th>");
+            echo("<th>Dział</th>");
+            while($row=$result->fetch_assoc()){ 
+            echo("<tr>");
+                echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["dzial"]."</td>");
+
+            echo("</tr>");
+        }
+
+        echo("</table>");
+  ?>
+
+</body>
+</html>
