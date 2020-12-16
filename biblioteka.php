b92783866fd574bfd7d9f672546719d3dc2ab4b9
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2020-12-16 21:17:37 +0000

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -1,25 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-<div class=nav>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>biblioteka</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="item colorRed">
+        Jakub Biliński 
+    </div>
+        <div class=nav>
+        <div class="item colorBlue">
     <a href="pracownicy.php">Pracownicy</a>
     <a href="organizacja.php">Pracownicy i Organizacja</a>
     <a href="funkcje.php">Funkcje Agregujące</a>
     <a href="dataczas.php">Data i Czas</a>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel">Github</a>
     <a href="formularz.html">Zakładka testowa</a>
     <a href="daneDoBazy.php">Dane Do Bazy</a>
-    <a href="biblioteka.php">biblioteka</a>
-    
+    <a href="biblioteka.php">Książki</a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel">Github</a>
+
+
 
 
 </div>
+<div class="item colorGreen">
 <?php
 
 require("connect.php");
@@ -79,6 +87,6 @@ echo("<h3>".$sql."</h3>");
 
 
 ?>
-    
+ </div>   
 </body>
 </html>
