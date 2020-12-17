d53357de8ec060e1bbbebb85ffabde0ddad5eb91
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2020-12-17 22:42:41 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -11,18 +11,17 @@
     <div class="nav">
         <a class="link a" href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel" >GITHUB</a>
             <a class="link e" href="index.php">Index</a>
-            <a class="link b" href="Pracownicy.php">Pracownicy</a>
-            <a class="link c" href="Pracownicy i Organizacja.php">Pracownicy i Organizacja</a>
-            <a class="link d" href="Funkcje agregujące.php">Funkcje agregujące</a>
-            <a class="link h" href="data i czas.php">Data i Czas</a>
+            <a class="link b" href="pracownicy.php">Pracownicy</a>
+            <a class="link c" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
+            <a class="link d" href="Funkcje Agreugjące.php">Funkcje agregujące</a>
+            <a class="link h" href="dataczas.php">Data i Czas</a>
             <a class="link z" href="formularz.html">Formularz</a>
             <a class="link y" href="daneDoBazy.php">DaneDoBazy</a>
             <a class="link g" href="książki.php">Książki</a>
         </div>
         <div class="item a">
-        <h1 class="h1zb">Maciej Dziendziel</h1>
-            <h2 class="h2za">Select * from pracownicy</h2>
-            <div class="">
+        <h1>Maciej Dziendziel</h1>
+            <h2>Select * from pracownicy</h2>
         <?php
                 require_once("connect.php");
                 $result=$conn->query("Select * from pracownicy");
@@ -39,10 +38,8 @@
                 echo("</table>");
         ?>
         </div>
-        </div>
         <div class="item b">
-            <h2 class="h2zb">Zad 2 Select * From pracownicy where imie like '%a' and dzial between 2 and 4</h2>
-            <div class="">
+            <h2>Zad 2 Select * From pracownicy where imie like '%a' and dzial between 2 and 4</h2>
         <?php
                 require_once("connect.php");
                 $result=$conn->query("Select * From pracownicy, organizacja where dzial=id_org and imie like '%a' and dzial between 2 and 4");
@@ -59,11 +56,9 @@
                             echo("</tr>");}
                 echo("</table>");
             ?>
-            </div>
         </div>
         <div class="item c">
             <h2 class=h2zc>Zad 3 Select avg(zarobki), dzial From pracownicy group by dzial</h2>
-            <div class="">
         <?php
                 require_once("connect.php");
                 $result=$conn->query("Select avg(zarobki), dzial, nazwa_dzial From pracownicy, organizacja where dzial=id_org group by dzial");
@@ -77,11 +72,9 @@
                             echo("</tr>");}
                 echo("</table>");
             ?>
-            </div>
         </div>
         <div class="item d">
             <h2>Zad 4 Select * From pracownicy where imie not like '%a' and zarobki between 15 and 50</h2>
-            <div class="">
         <?php
                 require_once("connect.php");
                 $result=$conn->query("Select * From pracownicy, organizacja where dzial=id_org and imie not like '%a' and zarobki between 15 and 50");
@@ -98,11 +91,9 @@
                             echo("</tr>");}
                 echo("</table>");
             ?>
-            </div>
         </div>
         <div class="item e">
             <h2>Zad 5 Select * From pracownicy where zarobki>15 and (dzial=1 or dzial=4)</h2>
-            <div class="">
         <?php
                 require_once("connect.php");
                 $result=$conn->query("Select * From pracownicy, organizacja where dzial=id_org and zarobki>15 and (dzial=1 or dzial=4)");
@@ -119,7 +110,7 @@
                             echo("</tr>");}
                 echo("</table>");
             ?>
-            </div>
+
         </div>
     </div>
 </body>
