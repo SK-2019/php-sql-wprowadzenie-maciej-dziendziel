95cebc6f62b77ca0ef30b31671ec8aa57057834c
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2020-12-17 23:13:27 +0100

    Add files via upload

--- ksiazki.php
+++ ksiazki.php
@@ -6,61 +6,21 @@
     <div class="item colorRed">
         <h1 class="title">Maciek Dziendziel</h1>
     </div>
-    <div class="item colorBlue">
-        <a href="orgPracownicy.php">Organizacja i Pracownicy</a>
-        <a href="agregat.php">Funkcje Agregujące</a>
-        <a href="pracownicy.php">pracownicy</a>
-        <a href="Data_i_Czas.php">Data i czas</a>
-        <a href="daneDoBazy.php">Dane do Bazy</a>
-        <a href="index.php">strona główna</a>
-        <a href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel">Github</a>
-    </div>
-    <div class="item colorGreen">
-<form action='wypozyczalnia.php' method='POST'>
-<?php
-
-
-require_once("connect.php");
-
-$sql = ("SELECT * from biblAutor");
-
-
-$result=$conn->query($sql);
-        echo("<select name='autor' id='autor'>");
-
-        while($wiersz=$result->fetch_assoc()) {
-               
-                    echo("<option value=".$wiersz['autor_id'].">".$wiersz["autor"]."</option>");}
-                    //echo("<input type='Submit' value='Submit'><br>");
-               
-            
-        echo("</select>");
-
-
-
-?>
-
+    <div class="nav">
+        <a class="link a" href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel" >GITHUB</a>
+            <a class="link b" href="index.php">Index</a>
+            <a class="link c" href="pracownicy.php">Pracownicy</a>
+            <a class="link d" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
+            <a class="link e" href="Funkcje Agreugjące.php">Funkcje agregujące</a>
+            <a class="link f" href="dataczas.php">Data i Czas</a>
+            <a class="link g" href="formularz.html">Formularz</a>
+            <a class="link h" href="daneDoBazy.php">DaneDoBazy</a>
+            <a class="link i" href="ksiazki.php">Książki</a>
+        </div>
+        <div class="item a">
 <?php
 require_once("connect.php");
-
-$sql = ("SELECT * from biblTytul");
-
-
-$result=$conn->query($sql);
-        echo("<select name='tytul' id='tytul'>");
-
-        while($wiersz=$result->fetch_assoc()) {
-               
-                    echo("<option value=".$wiersz['tytul_id'].">".$wiersz["tytul"]."</option>");}
-                    echo("<input type='Submit' value='Submit'><br>");
-               
-            
-        echo("</select>");
-?>
-</form>
-<?php
-require_once("connect.php");
-$sql = ("SELECT * from biblAutor, biblTytul, biblAutor_biblTytul where biblAutor_id=autor_id and biblTytul_id=tytul_id");
+$sql ="Select * From biblAutor, biblTytul, biblAutor_biblTytul where biblAutor_id=biblAutor.id and biblTytul_id=biblTytul.id";
 echo("<li>".$sql);
 $result = $conn->query($sql);
 echo("<table border=0>");
@@ -83,5 +43,5 @@ while($row=$result->fetch_assoc()) {
 echo("</table>");
 
 ?>
-    </div>
+</div>
 </div>
