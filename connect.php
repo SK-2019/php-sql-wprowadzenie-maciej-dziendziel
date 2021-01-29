76b513068868bd229d9f0ca1da3499c76184f73f
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-01-29 08:45:22 +0000

    commit2
    1

--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $conn= new mysqli("mysql-mlodychopak68.alwaysdata.net","221525","Artur072013","mlodychopak68_db");
-    if ($conn->connect_error) {
-        die("connection failed: ".mysqli_connect_error());
-    }
-?>
---
+++ inne/connect.php
@@ -0,0 +1,6 @@
+<?php
+    $conn= new mysqli("mysql-mlodychopak68.alwaysdata.net","221525","Artur072013","mlodychopak68_db");
+    if ($conn->connect_error) {
+        die("connection failed: ".mysqli_connect_error());
+    }
+?>
