7a41e2a5f3358580f1634dadd687ed3160155e7a
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-02-03 19:22:46 +0000

    Update pracownicyiorganizacja.php

--- pracownicy/pracownicyiorganizacja.php
+++ pracownicy/pracownicyiorganizacja.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
     <div class="con">
@@ -22,7 +22,7 @@
 <div class="item a">
                <h2 class="h2za">SELECT * FROM pracownicy, organizacja WHERE dzial=id_org</h2>
         <?php
-                require_once("connect.php");
+                require_once("../inne/connect.php");
                 $result=$conn->query("Select * From pracownicy, organizacja where dzial=id_org");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -40,7 +40,7 @@
         ?>
             <h2 class="hz1">SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND (dzial=1 or dzial=4)</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("Select * From pracownicy, organizacja where dzial=id_org and (dzial=1 or dzial=4)");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -57,7 +57,7 @@
             ?>
             <h2 class=hz1>SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND imie like '%a'</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("Select * From pracownicy, organizacja where dzial=id_org and imie like '%a'");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -74,7 +74,7 @@
             ?>
             <h2 class="hz1">SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND imie not like '%a'</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("Select * From pracownicy, organizacja where dzial=id_org and imie not like '%a'");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -91,7 +91,7 @@
             ?>
             <h2 class="hz1">SELECT * FROM pracownicy, organizacja WHERE dzial=id_org ORDER BY imie desc</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("Select * From pracownicy, organizacja where dzial=id_org order by imie desc");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -108,7 +108,7 @@
             ?>
             <h2 class="hz1">SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND dzial=3 ORDER BY imie asc</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("Select * From pracownicy, organizacja where dzial=id_org and dzial=3 order by imie asc");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -125,7 +125,7 @@
             ?>
             <h2 class="hz1">SELECT * FROM pracownicy,organizacja WHERE id_org=dzial AND imie NOT LIKE '%a' ORDER BY imie asc</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("SELECT * FROM pracownicy,organizacja WHERE id_org=dzial and imie NOT LIKE '%a' order by imie asc");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -142,7 +142,7 @@
             ?>
                 <h2 class="hz1">SELECT * FROM pracownicy,organizacja WHERE id_org=dzial AND imie NOT LIKE '%a' AND (dzial = 1 OR dzial=3) ORDER BY zarobki asc</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("SELECT * FROM pracownicy,organizacja WHERE id_org=dzial and imie NOT LIKE '%a' and (dzial = 1 OR dzial=3) order by zarobki asc");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -159,7 +159,7 @@
             ?>
                 <h2 class="hz1">SELECT * FROM pracownicy,organizacja WHERE id_org=dzial AND imie NOT LIKE '%a' ORDER BY nazwa_dzial asc, zarobki asc</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("SELECT * FROM pracownicy,organizacja WHERE id_org=dzial and imie NOT LIKE '%a' order by  nazwa_dzial asc, zarobki asc");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -176,7 +176,7 @@
             ?>  
                   <h2 class="hz1">SELECT * FROM pracownicy,organizacja WHERE id_org=dzial AND imie NOT LIKE '%a' ORDER BY nazwa_dzial asc, zarobki asc</h2>
         <?php
-                require_once("connect.php");
+               
                 $result=$conn->query("SELECT * FROM pracownicy,organizacja WHERE id_org=dzial and imie NOT LIKE '%a' order by  nazwa_dzial asc, zarobki asc");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -193,7 +193,7 @@
             ?>
                   <h2 class="h2zk">SELECT * FROM pracownicy,organizacja WHERE id_org=dzial AND dzial=4 ORDER BY zarobki desc LIMIT 2</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("SELECT * FROM pracownicy,organizacja WHERE id_org=dzial AND dzial=4 ORDER BY zarobki desc LIMIT 2");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -210,7 +210,7 @@
             ?>     
                   <h2 class="hz1">SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND imie LIKE '%a' AND (dzial=2 or dzial=4) ORDER BY zarobki desc limit 3</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND imie LIKE '%a' AND (dzial=2 or dzial=4) ORDER BY zarobki desc limit 3");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
@@ -227,7 +227,7 @@
             ?> 
                   <h2 class="hz1">SELECT * FROM pracownicy, organizacja WHERE dzial=id_org ORDER BY data_urodzenia asc limit 1</h2>
         <?php
-                require_once("connect.php");
+                
                 $result=$conn->query("Select * From pracownicy, organizacja where dzial=id_org order by data_urodzenia asc limit 1");
                 echo("<table border=1>");
                     echo("<th>ID</th>");
