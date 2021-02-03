b359c285303df4e26df85c98ebc7c6030d621204
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-02-03 19:14:09 +0000

    Update daneDoBazy.php

--- formularze/daneDoBazy.php
+++ formularze/daneDoBazy.php
@@ -10,14 +10,14 @@
     <div class="con">
     <div class="nav">
         <a class="link a" href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel" >GITHUB</a>
-            <a class="link b" href="index.php">Index</a>
-            <a class="link c" href="pracownicy.php">Pracownicy</a>
-            <a class="link d" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
-            <a class="link e" href="Funkcje Agreugjące.php">Funkcje agregujące</a>
-            <a class="link f" href="dataczas.php">Data i Czas</a>
+            <a class="link b" href="/index.php">Index</a>
+            <a class="link c" href="/pracownicy/pracownicy.php">Pracownicy</a>
+            <a class="link d" href="/pracownicy/pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
+            <a class="link e" href="/funkcje/Funkcje Agreugjące.php">Funkcje agregujące</a>
+            <a class="link f" href="/inne/dataczas.php">Data i Czas</a>
             <a class="link g" href="formularz.html">Formularz</a>
-            <a class="link h" href="daneDoBazy.php">DaneDoBazy</a>
-            <a class="link i" href="ksiazki.php">Książki</a>
+            <a class="link h" href="/formularze/daneDoBazy.php">DaneDoBazy</a>
+            <a class="link i" href="/bibliotekapliki/ksiazki.php">Książki</a>
         </div>
         <div class="item a">
             <h1>Dodaj Pracownika</h1>
