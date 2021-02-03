3ef64c4d85d47a797c3fb6ac352702754656ef46
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-02-03 20:00:11 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Maciej Dziendziel 2ti gr1</title>
     <link rel="stylesheet" href="/style.css">
+  <link rel="icon" href="https://i.kym-cdn.com/photos/images/facebook/000/735/052/513.jpg%22%3E%3E>
 </head>
 <body>
     <div class="con">
