d26ef0d1f1785bad74ff6fdbac2be4f30dcfae96
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-02-24 21:18:08 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$_SERVER['servername'] = "cos";
-$_SERVER['password'] = "cos";
-$_SERVER['username'] = "cos";
-$_SERVER['dbname'] = "cos";
+$_SERVER['servername'] = "mysql-mlodychopak68.alwaysdata.net";
+$_SERVER['password'] = "Artur072013";
+$_SERVER['username'] = "221525";
+$_SERVER['dbname'] = "mlodychopak68_db";
 ?>
