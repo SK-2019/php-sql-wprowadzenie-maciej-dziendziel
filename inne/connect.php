dde73375fbbc430d2b1724489b4ebc99f1468b4f
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-02-24 21:11:28 +0000

    Update connect.php

--- inne/connect.php
+++ inne/connect.php
@@ -1,6 +1,9 @@
 <?php
-    $conn= new mysqli("mysql-mlodychopak68.alwaysdata.net","221525","Artur072013","mlodychopak68_db");
+
+    $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
     if ($conn->connect_error) {
         die("connection failed: ".mysqli_connect_error());
     }
+
+
 ?>
