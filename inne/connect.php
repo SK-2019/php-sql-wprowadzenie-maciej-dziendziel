adb29c68f3ad0e8ed5c0b5b380ebaec2d531a341
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-02-25 21:59:22 +0000

    Delete connect.php

--- inne/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
-    if ($conn->connect_error) {
-        die("connection failed: ".mysqli_connect_error());
-    }
-
-
-?>
