6b963b557c2e4cf8642930a37e333fb42f230f67
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-03-29 15:35:03 +0000

    Update pracownicyiorganizacja.php

--- pracownicy/pracownicyiorganizacja.php
+++ pracownicy/pracownicyiorganizacja.php
@@ -14,7 +14,7 @@
                 </div>
     <div class="nav">
         <a class="menu" href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel" >GITHUB</a>
-        <a class="menu" href="index.php">Index</a>
+        <a class="menu" href="/index.php">Index</a>
         <a class="menu" href="/pracownicy/pracownicy.php">Pracownicy</a>
         <a class="menu" href="/pracownicy/pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
         <a class="menu" href="/funkcje/Funkcje Agreugjące.php">Funkcje agregujące</a>
@@ -22,7 +22,7 @@
         <a class="menu" href="/formularz.html">Formularz</a>
         <a class="menu" href="/formularze/daneDoBazy.php">DaneDoBazy</a>
         <a class="menu" href="/bibliotekapliki/ksiazki.php">Książki</a>
-        <a class="menu" href="flexbox.html">flexbox</a>
+        <a class="menu" href="/flexbox.html">flexbox</a>
     </div>
         <div class="tresc">
             <?php
