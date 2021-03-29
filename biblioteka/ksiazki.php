4f82e02990cea9159c5623718f00305b75f010a2
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-03-29 16:20:33 +0000

    Update ksiazki.php

--- biblioteka/ksiazki.php
+++ biblioteka/ksiazki.php
@@ -1,25 +1,32 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="/style.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maciej Dziendziel 2ti gr1</title>
+    <link rel="stylesheet" href="/assets/style.css">
+  <link rel="icon" href="https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png">
     </head>
-<div class="container">
-    <div class="item colorRed">
-        <h1 class="title">Maciek Dziendziel</h1>
-    </div>
+    <body>
+    <div class="pole">
+                <div class="naglowek">
+                <h1>Maciej Dziendziel</h1>
+                </div>
     <div class="nav">
-         <a class="link a" href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel" >GITHUB</a>
-            <a class="link b" href="/index.php">Index</a>
-            <a class="link c" href="/pracownicy/pracownicy.php">Pracownicy</a>
-            <a class="link d" href="/pracownicy/pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
-            <a class="link e" href="/funkcje/Funkcje Agreugjące.php">Funkcje agregujące</a>
-            <a class="link f" href="/inne/dataczas.php">Data i Czas</a>
-            <a class="link g" href="/formularz.html">Formularz</a>
-            <a class="link h" href="/formularze/daneDoBazy.php">DaneDoBazy</a>
-            <a class="link i" href="/bibliotekapliki/ksiazki.php">Książki</a>
-        </div>
-        <div class="item a">
-<?php
-require_once("../inne/connect.php");
+        <a class="menu" href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel" >GITHUB</a>
+        <a class="menu" href="index.php">Index</a>
+        <a class="menu" href="/pracownicy/pracownicy.php">Pracownicy</a>
+        <a class="menu" href="/pracownicy/pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
+        <a class="menu" href="/funkcje/Funkcje Agreugjące.php">Funkcje agregujące</a>
+        <a class="menu" href="dataczas.php">Data i Czas</a>
+        <a class="menu" href="/formularz.html">Formularz</a>
+        <a class="menu" href="/formularze/daneDoBazy.php">DaneDoBazy</a>
+        <a class="menu" href="/bibliotekapliki/ksiazki.php">Książki</a>
+        <a class="menu" href="flexbox.html">flexbox</a>
+    </div>
+        <div class="tresc">
+        <?php
+require_once("../connect.php");
 $sql ="Select * From biblAutor, biblTytul, biblAutor_biblTytul where biblAutor_id=biblAutor.id and biblTytul_id=biblTytul.id";
 echo("<li>".$sql);
 $result = $conn->query($sql);
@@ -43,5 +50,7 @@ while($row=$result->fetch_assoc()) {
 echo("</table>");
 
 ?>
-</div>
-</div>
+      </div>
+    </div>
+</body>
+</html>
