5d8868a27ddca7c4b0cd722c844954559bdfd2a7
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-04-20 07:54:50 +0000

    Update strona.php

--- formularze/strona.php
+++ formularze/strona.php
@@ -8,15 +8,7 @@
 </head>
 <body>
 <div class="nav">
-         <a class="link a" href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel" >GITHUB</a>
-            <a class="link b" href="/index.php">Index</a>
-            <a class="link c" href="/pracownicy/pracownicy.php">Pracownicy</a>
-            <a class="link d" href="/pracownicy/pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
-            <a class="link e" href="/funkcje/Funkcje Agreugjące.php">Funkcje agregujące</a>
-            <a class="link f" href="/inne/dataczas.php">Data i Czas</a>
-            <a class="link g" href="/formularz.html">Formularz</a>
-            <a class="link h" href="/formularze/daneDoBazy.php">DaneDoBazy</a>
-            <a class="link i" href="/bibliotekapliki/ksiazki.php">Książki</a>
+         <?php include("../menu.php") ?>
         </div>
 <div class="formularz">
 <?php
