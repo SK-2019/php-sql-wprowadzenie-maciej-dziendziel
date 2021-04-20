721f9ca39e657cc334c6f459d77b5c6ae8792fe4
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-04-20 07:54:12 +0000

    Update daneDoBazy.php

--- formularze/daneDoBazy.php
+++ formularze/daneDoBazy.php
@@ -13,16 +13,7 @@
                 <h1>Maciej Dziendziel</h1>
                 </div>
     <div class="nav">
-        <a class="menu" href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel" >GITHUB</a>
-        <a class="menu" href="/index.php">Index</a>
-        <a class="menu" href="/pracownicy/pracownicy.php">Pracownicy</a>
-        <a class="menu" href="/pracownicy/pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
-        <a class="menu" href="/funkcje/Funkcje Agreugjące.php">Funkcje agregujące</a>
-        <a class="menu" href="/dataczas.php">Data i Czas</a>
-        <a class="menu" href="/formularz.html">Formularz</a>
-        <a class="menu" href="/formularze/daneDoBazy.php">DaneDoBazy</a>
-        <a class="menu" href="/biblioteka/ksiazki.php">Książki</a>
-        <a class="menu" href="/flexbox.html">flexbox</a>
+        <?php include("../menu.php") ?>
     </div>
         <div class="tresc">
             <div class="item a">
