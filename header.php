a190a1f5935e6093c47f88d65c495e285ec9cdd9
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-04-25 16:06:37 +0000

    Update header.php

--- header.php
+++ header.php
@@ -1,2 +1,2 @@
 <h1 class="title">Maciej Dziendziel</h1>
-<a class="click" href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">Notion SO</a>
+<a class="push" href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">Notion SO</a>
