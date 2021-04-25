aed4ead2c583ebd09ac8ae6cb21994f869ac1cda
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-04-25 18:48:09 +0200

    pop

--- assets/menu.php
+++ assets/menu.php
@@ -1,5 +1,5 @@
         <a class="menu" href="https://github.com/SK-2019/php-sql-wprowadzenie-maciej-dziendziel" >GITHUB</a>
-        <a class="menu" href="index.php">Index</a>
+        <a class="menu" href="/index.php">Index</a>
         <a class="menu" href="/pracownicy/pracownicy.php">Pracownicy</a>
         <a class="menu" href="/pracownicy/pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
         <a class="menu" href="/funkcje/Funkcje Agreugjące.php">Funkcje agregujące</a>
