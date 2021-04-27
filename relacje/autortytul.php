1635e1cdf53d985c08c83ef0b0c25c1c75ad6964
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-04-27 11:03:09 +0200

    pop

---
+++ relacje/autortytul.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maciej Dziendziel 2ti gr1</title>
+    <link rel="stylesheet" href="/assets/style.css">
+  <link rel="icon" href="https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png">
+    </head>
+    <body>
+    <div class="pole">
+                <div class="naglowek">
+                 <?php include("../assets/header.php") ?>
+                </div>
+    <div class="nav">
+        <?php include("../assets/menu.php") ?>
+    </div>
+    <div class="tresc">
+<?php
+                echo("<h1>Biblioteka</h1>");
+
+
+                require_once("../connect.php");
+
+                $sql = ("SELECT * FROM autor");
+                echo("<h2>".$sql."</h2>");
+                $result=$conn->query($sql);
+                
+                        echo("<table border=1>");
+                        echo("<th>id</th>");
+                        echo("<th>autor</th>");
+    
+
+                        while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["id"]."</td><td>".$row["nazwisko"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+
+
+                        $sql = ("SELECT * FROM tytul");
+                echo("<h2>".$sql."</h2>");
+                $result=$conn->query($sql);
+                
+                        echo("<table border=1>");
+                        echo("<th>id</th>");
+                        echo("<th>tytul</th>");
+    
+
+                        while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["id"]."</td><td>".$row["tytul"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+
+
+                $sql = ("SELECT * FROM autor, tytul, autor_tytul  where autor_id = autor.id and tytul_id = tytul.id");
+                echo("<h2>".$sql."</h2>");
+                $result=$conn->query($sql);
+                
+                        echo("<table border=1>");
+                        echo("<th>autor</th>");
+                        echo("<th>tytul</th>");
+    
+
+                        while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["nazwisko"]."</td><td>".$row["tytul"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+
+
+                        ?>
+                        </div>
---
+++ relacje/fryzjerklient.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maciej Dziendziel 2ti gr1</title>
+    <link rel="stylesheet" href="/assets/style.css">
+  <link rel="icon" href="https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png">
+    </head>
+    <body>
+    <div class="pole">
+                <div class="naglowek">
+                 <?php include("../assets/header.php") ?>
+                </div>
+    <div class="nav">
+        <?php include("../assets/menu.php") ?>
+    </div>
+    <div class="tresc">
+<?php
+
+            require_once("../connect.php");
+                echo("<h1>SPA</h1>");
+
+                $sql = ("SELECT * FROM fryzjer");
+                echo("<h2>".$sql."</h2>");
+                $result=$conn->query($sql);
+                        echo("<table border=1>");
+                        echo("<th>id</th>");
+                        echo("<th>fryzjer</th>");
+
+                        while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["id"]."</td><td>".$row["fryzjer"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+
+                
+                $sql = ("SELECT * FROM klient");
+                echo("<h2>".$sql."</h2>");
+                $result=$conn->query($sql);
+                        echo("<table border=1>");
+                        echo("<th>id</th>");
+                        echo("<th>klient</th>");
+
+                        while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["id"]."</td><td>".$row["klient"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+
+
+                        $sql = ("SELECT * FROM fryzjer, klient, fryzjer_klient where fryzjer_id = fryzjer.id and klient_id = klient.id");
+                        echo("<h2>".$sql."</h2>");
+                        $result=$conn->query($sql);
+                                echo("<table border=1>");
+                                echo("<th>fryzjer</th>");
+                                echo("<th>klient</th>");
+        
+                                while($row=$result->fetch_assoc()) {
+                                        echo("<tr>");
+                                            echo("<td>".$row["fryzjer"]."</td><td>".$row["klient"]."</td>");
+                                        echo("</tr>");
+                                    }
+                                echo("</table>");
+
+        ?>
+        </div>
