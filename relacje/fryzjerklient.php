a445852c3d5e1d0c6bbef45a6f4c49b9f452556d
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-05-10 16:13:03 +0200

    pop

--- relacje/fryzjerklient.php
+++ relacje/fryzjerklient.php
@@ -55,6 +55,7 @@
                         echo("<h2>".$sql."</h2>");
                         $result=$conn->query($sql);
                                 echo("<table border=1>");
+                                echo("<th>id</th>");
                                 echo("<th>fryzjer</th>");
                                 echo("<th>klient</th>");
         
