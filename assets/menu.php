ecc15be49c34b68746f31e24180a01ec10dd60eb
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-05-11 10:08:01 +0200

    test

--- assets/menu.php
+++ assets/menu.php
@@ -12,4 +12,5 @@
         <a class="menu" href="/relacje/producentprodukt.php">Producent produkt</a>
         <a class="menu" href="/relacje/autortytul.php">Autor tytul</a>
         <a class="menu" href="/relacje/fryzjerklient.php">Fryzjer klient</a>
+        <a class="menu" href="pic.html">pictures</a>
 
