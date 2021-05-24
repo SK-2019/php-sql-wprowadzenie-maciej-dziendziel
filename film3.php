bd638d4adb601b754d0ee13f52482aad999dc86f
maciej-dziendziel <73523395+maciej-dziendziel@users.noreply.github.com>
2021-05-24 22:43:42 +0200

    zad

--- film3.php
+++ film3.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Maciej Dziendziel 2ti gr1</title>
+    <link rel="stylesheet" href="style.css"/>
+  <link rel="icon" href="https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png"/>
+    </head>
+    <body>
+    <div class="pole">
 <?php
 for($i=1:$i<11:$i++){
     echo($i.'maciek<br>');
 }
 ?>
+</div>
+</body>
+</html>
